10fe6a0c7870e5e7e5d274b926981b6d77769692
Szigetvari Simon <szigetvari.simon@gmail.com>
2024-03-28 22:13:53 +0100

    modifications

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ def health():
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!!"
+    return "Mission Failed Successfully! 🎉🔬🚀✨🎈🔭🧪🎉"
  
 if __name__ == '__main__':  
    app.run(host="0.0.0.0", port=5000)
